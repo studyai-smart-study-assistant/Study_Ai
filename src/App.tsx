651fd0e2820c064dfeaaf2076e52e60dd43f7ea8
ajit91884270 <ajit91884270@gmail.com>
2026-03-06 12:58:17 +0000

    Feat: Add React Router future flag to resolve v7 warning

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function App() {
                   <NotificationProvider>
                     <TooltipProvider>
                       <div className="min-h-screen bg-background">
-                        <Routes>
+                        <Routes future={{ v7_relativeSplatPath: true }}>
                           {/* Routes wrapped in persistent AppShell */}
                           <Route element={<AppShell />}>
                             <Route path="/" element={<PageWrapper><Index /></PageWrapper>} />
